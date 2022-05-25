4c5e9f743f52c925c08145b5464b74caad9491b5
pocpocpo <92831222+pocpocpo@users.noreply.github.com>
2022-05-25 14:46:01 -0300

    stage: include fibonacci function

--- math-module.wasm.test.js
+++ math-module.wasm.test.js
@@ -20,3 +20,7 @@ test('square 5', () => expect(mathModule.square(5)).toBe(25))
 test('add 5 to 5', () => expect(mathModule.add(5, 5)).toBe(10))
 test('subtract 3 from 5', () => expect(mathModule.sub(5, 3)).toBe(2))
 
+test('fibonacci of 0', () => expect(mathModule.fibonacci(0)).toBe(0))
+test('fibonacci of 1', () => expect(mathModule.fibonacci(1)).toBe(1))
+test('fibonacci of 19', () => expect(mathModule.fibonacci(19)).toBe(4181))
+test('fibonacci of 20', () => expect(mathModule.fibonacci(20)).toBe(6765))
